710d3edadfbeeecb71772df27b53e6ee1f50cfc9
Carolln_X <549129236@qq.com>
2018-06-03 16:11:27 +0800

    add charts to professor-info page

---
+++ src/routes/professor-info/components/info.jsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  padding-left: 15%;
+  padding-right: 15%;
+  padding-top: 30px;
+`
+
+export const UserImage = styled.div`
+  margin-bottom: 40px;
+  margin-right: 20px;
+  width: 35%;
+  height: auto;
+  float: left;
+`
+export const UserInfo = styled.div`
+  margin-bottom: 40px;
+  margin-left: 20px;
+  width: 55%;
+  height: auto;
+  float: left;
+`
+export const LeftTable = styled.div`
+  width: 50%;
+  height: auto;
+  float: left;
+`
+export const RightTable = styled.div`
+  width: 50%;
+  height: auto;
+  float: left;
+`
